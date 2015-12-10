4414d7f7ca7fc7d2ef01b0c3ed877b5b2108ac62
ovrmrw <t.ngc.0403@gmail.com>
2015-12-10 10:31:18 +0900

    20151210-2

--- src/my.d.ts
+++ src/my.d.ts
@@ -1,25 +1,4 @@
 ///////////////////////////////////////////////////
-// import xxx from 'xxx' の構文が書けるように拡張
-declare module "lodash" {
-  export default _;
-}
-
-declare module 'moment' {
-  export default moment;
-}
-
-declare module "numeral" {
-  export default numeral;
-}
-
-declare module "prominence" {
-  var prominence: any;
-  export default prominence;
-}
-
-// Expressはexpress.d.tsの最後を直接書き換える。
-
-///////////////////////////////////////////////////
 // Electronのレンダラプロセスでrequire('remote')をSystem._nodeRequire('remote')と書くため
 declare interface System {
   _nodeRequire: (module: string) => any;
